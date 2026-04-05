cf8c7b7b5fb529f71c56867db957f5e51a9c56a3
Shashank <shashank.sule19@gmail.com>
2026-04-05 20:34:34 +0530

    Implement 3D Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Product from './components/ProductViewer'
 
 const App = () => {
   return (
     <main>
-
       <Navbar />
       <Hero />
-
+      <Product />
     </main>
   )
 }
